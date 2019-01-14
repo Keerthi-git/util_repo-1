8dd513fa31465ac7e1b658dd247e7159407f5dc5
Niranjan Shrishail Hampannavar <M1023294@mindtree.com>
2019-01-14 10:55:48 +0530

    commit

--- java/java-ci-cd.groovy
+++ java/java-ci-cd.groovy
@@ -7,20 +7,24 @@ def execute() {
 		version=pom.version
 		echo 'read success'
 	}
+	
 	/*stage('scan') {
         sh props.SONAR_SCAN+' '+props.SONAR_HOST
 		echo 'scan success'
 	}*/
+	
 	stage('build') {
         sh props.MAVEN_BUILD
 		echo 'build success'
     }
+	
 	stage('upload') {
         script {
 			commonUtility.uploadWarArtifactory();
 			echo 'upload success'
 		}
     }
+	
 	stage('deploy') {
         sh props.TOMCAT_DEPLOY+' '+props.TOMCAT_LOCATION
 		echo 'deploy success'
